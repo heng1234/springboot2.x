4b5daa39d8b7389931f139fd85b70bf12f5fea7d
kaifa <1350047452@qq.com>
2019-10-18 13:35:34 +0800

    delete

---
+++ boot_banner_yml/src/main/java/com/boot/boot_banner_yml/BootBannerYmlApplication.java
@@ -0,0 +1,13 @@
+package com.boot.boot_banner_yml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBannerYmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootBannerYmlApplication.class, args);
+    }
+
+}
